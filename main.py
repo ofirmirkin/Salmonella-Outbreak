5be6f3f6045e823432567c6007ad98040c725425
Ofir MIRKIN <60558136+ofirmirkin@users.noreply.github.com>
2024-10-20 01:15:25 +0200

    Typo

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Python tool for finding mutations in a genome sequence using k-mer analysis and BLAST search.
 BLAST hits are saved to XML files for further analysis.
 
-Usage: python main.py <wild_filename> <resistant_filename>
+Usage: python3 main.py <wild_filename> <resistant_filename>
 """
 
 from collections import defaultdict
